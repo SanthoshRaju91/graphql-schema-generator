7f2264de213d8a6ae86318228efd667d8b88e20d
santhoshRaju91 <santhosh.codeholic@gmail.com>
2021-10-16 12:48:01 +0530

    Completion commit

--- src/tests/index.ts
+++ src/tests/index.ts
@@ -1,4 +1,5 @@
-import { GraphQLServer } from "graphql-yoga";
+import express from "express";
+import graphqlHTTP from 'express-graphql';
 import buildSchema from "../core";
 
 const schema = {
@@ -14,12 +15,40 @@ const schema = {
     }
 };
 
-buildSchema(schema)
-    .then(builtSchema => {
-        const server = new GraphQLServer({
-            schema: builtSchema
-        });
+const app = express();
+app.use("/graphql", graphqlHTTP(async (request, response, graphQLParams) => ({
+    schema: await buildSchema(schema),
+    graphiql: true
+})));
+app.listen(4000, (err) => {
+    if(err) 
+        console.error(err);
+    console.log("Server is running on port 4000");
+})
 
-        server.start(() => console.log(`Server is running on port 4000`));
-    })
-    .catch(err => console.error(err));
+// buildSchema(schema)
+//     .then(builtSchema => {
+//         const app = express();
+//         app.use("/graphql", graphqlHTTP(async (request, response, graphQLParams) => ({
+//             schema: builtSchema,
+//             graphiql: true
+//         })))
+//         app.listen(4000, (err) => {
+//             if(err) 
+//                 console.error(err);
+//             console.log("Server is running on port 4000")
+//         });
+//     });
+// import { GraphQLServer } from "graphql-yoga";
+// import buildSchema from "../core";
+
+
+
+// buildSchema(schema)
+//     .then(builtSchema => {
+//         const server = new GraphQLServer({
+//             schema: builtSchema
+//         });
+//         server.start(() => console.log(`Server is running on port 4000`));
+//     })
+//     .catch(err => console.error(err));
